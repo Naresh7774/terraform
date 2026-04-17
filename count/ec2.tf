3caccaf8e6f5bd99de9f107f8619ee88e2ae64a7
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-17 23:17:27 +0530

    project

--- count/ec2.tf
+++ count/ec2.tf
@@ -7,3 +7,8 @@ resource "aws_instance" "terraform" {
     tags = {
         Name = var.instances[count.index]
         Terraform = "true"
+        Project = "roboshop"
+    }
+}
+
+resource "aws_security_group" "allow_all" {
