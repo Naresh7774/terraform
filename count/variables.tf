857a9c048de35674b2b81c5171f019ac41fcffd7
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-17 23:20:04 +0530

    project

--- count/variables.tf
+++ count/variables.tf
@@ -2,3 +2,7 @@ variable "instances" {
     default = [ "mongodb", "redis", "mysql", "rabbitmq", "catalogue", "user", "cart", "shipping", "payment", "frontend" ]
 }
 
+variable "zone_id" {
+    default = "Z0948150OFPSYTNVYZOY"
+}
+
