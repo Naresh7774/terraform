d9674bda7286f45e57ed17664334ce8509a51645
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-17 23:17:16 +0530

    project

--- count/ec2.tf
+++ count/ec2.tf
@@ -4,3 +4,6 @@ resource "aws_instance" "terraform" {
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_all.id]
+    tags = {
+        Name = var.instances[count.index]
+        Terraform = "true"
