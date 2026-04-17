0299b88e53004d00f962897aa3da14f8602f9e23
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-17 23:19:31 +0530

    project

--- count/r53.tf
+++ count/r53.tf
@@ -1,3 +1,5 @@
 resource "aws_route53_record" "roboshop" {
   count = length(var.instances)
   zone_id = "${var.zone_id}"
+  name    = "${var.instances[count.index]}.${var.domain_name}" # mongodb.daws86s.fun
+  type    = "A"
