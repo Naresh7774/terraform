f284ee028229fd5d32376aa8e94c96245d85e929
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-18 23:43:08 +0530

    project

--- data-sources/ec2.tf
+++ data-sources/ec2.tf
@@ -15,3 +15,7 @@
 #     from_port        = 0 # from port 0 to to port 0 means all ports
 #     to_port          = 0 
 #     protocol         = "-1" # -1 means all protocols
+#     cidr_blocks      = ["0.0.0.0/0"] # internet
+#   }
+
+#   ingress {
