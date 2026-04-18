a5688e5ced301685543ef7d93c5d76356c5511f5
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-18 23:44:04 +0530

    project

--- dynamic/sg.tf
+++ dynamic/sg.tf
@@ -2,3 +2,6 @@ resource "aws_security_group" "roboshop_instance" {
   name   = "roboshop-strict-sg"
 
   egress {
+    from_port        = 0 # from port 0 to to port 0 means all ports
+    to_port          = 0 
+    protocol         = "-1" # -1 means all protocols
