51d6952b7217bd9eb97f53c95419e54d723795f3
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-18 23:44:12 +0530

    project

--- dynamic/sg.tf
+++ dynamic/sg.tf
@@ -5,3 +5,8 @@ resource "aws_security_group" "roboshop_instance" {
     from_port        = 0 # from port 0 to to port 0 means all ports
     to_port          = 0 
     protocol         = "-1" # -1 means all protocols
+    cidr_blocks      = ["0.0.0.0/0"] # internet
+  }
+  
+  # block
+  dynamic "ingress" {
