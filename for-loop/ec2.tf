cd3352afef79a81afbe0c4fdfcede9920fe56898
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-19 22:59:01 +0530

    project

--- for-loop/ec2.tf
+++ for-loop/ec2.tf
@@ -6,3 +6,12 @@ resource "aws_instance" "terraform" {
     instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_all.id]
     tags = {
+        #Name = each.key
+        Name = each.value
+        Terraform = "true"
+    }
+}
+
+resource "aws_security_group" "allow_all" {
+  name   = "allow-all"
+
