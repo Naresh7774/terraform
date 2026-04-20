37003e9bec63074ea544a5e1914494a75a8b5d47
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-20 22:45:47 +0530

    project

--- locals/variables.tf
+++ locals/variables.tf
@@ -6,3 +6,11 @@ variable "environment"{
     default = "dev"
 }
 
+# variable "common_name" {
+#     default = "${var.project}-${var.environment}"
+# }
+
+variable "ami_id" {
+    type = string
+    default     = "ami-09c813fb71547fc4f"
+}
