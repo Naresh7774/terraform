c94685c5bdb798c4486a6386f6f54340e26e9738
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-20 22:44:23 +0530

    project

--- locals/ec2.tf
+++ locals/ec2.tf
@@ -5,3 +5,13 @@ resource "aws_instance" "terraform" {
     tags = local.ec2_tags
 }
 
+resource "aws_security_group" "allow_all" {
+  name   = "${local.common_name}-allow-all"
+
+  egress {
+    from_port        = var.egress_from_port
+    to_port          = var.egress_to_port
+    protocol         = "-1" # -1 means all protocols
+    cidr_blocks      = var.cidr
+  }
+
